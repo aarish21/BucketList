2fe1759c100f5687b9ef0f7cec3c2b24e4de800a
aarish <aarish.rahman21@gmail.com>
2022-04-25 19:52:29 +0530

    started working on the project

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,11 +6,46 @@
 //
 
 import SwiftUI
+import LocalAuthentication
+
+
 
 struct ContentView: View {
+    @State private var isUnlocked = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            if isUnlocked{
+                Text("unlocked")
+            }else{
+                Text("locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    
+      
+    }
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data."
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                if success {
+                    // authenticated successfully
+                    isUnlocked = true
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometrics
+            isUnlocked = true
+        }
     }
 }
 
