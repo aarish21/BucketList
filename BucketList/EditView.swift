020abd73a393bf5feea2caddad28ee38612330d0
aarish <aarish.jerry@gmail.com>
2022-05-01 20:29:39 +0530

    completed day 73 challenge added alert on failure of biometric, added viewModel for EditView & made + button better

--- BucketList/EditView.swift
+++ BucketList/EditView.swift
@@ -8,30 +8,24 @@
 import SwiftUI
 
 struct EditView: View {
-    enum LoadingState : Equatable{
-        case loading,loaded,failed
-        
-    }
-    @Environment(\.dismiss) var dismiss
-    var location: Location
     
-    @State private var name: String
-    @State private var description: String
-    @State private var loadingState = LoadingState.loading
-    @State private var pages = [Page]()
+    @Environment(\.dismiss) var dismiss
+    
+    @StateObject private var viewModel: ViewModel
+    
     var onSave: (Location) -> Void
     
     var body: some View {
         NavigationView{
             Form{
                 Section{
-                    TextField("Place name", text: $name)
-                    TextField("Description", text: $description)
+                    TextField("Place name", text: $viewModel.name)
+                    TextField("Description", text: $viewModel.description)
                 }
                 Section("Nearby..."){
-                    switch loadingState {
+                    switch viewModel.loadingState {
                     case .loaded:
-                        ForEach(pages, id: \.pageid) { page in
+                        ForEach(viewModel.pages, id: \.pageid) { page in
                             Text(page.title)
                                 .font(.headline)
                             + Text(": ") +
@@ -57,47 +51,24 @@ struct EditView: View {
             .navigationTitle("Place details")
             .toolbar {
                 Button("Save"){
-                    var newLocation = location
+                    var newLocation = viewModel.location
                     newLocation.id = UUID()
-                    newLocation.name = name
-                    newLocation.description = description
+                    newLocation.name = viewModel.name
+                    newLocation.description = viewModel.description
                     onSave(newLocation)
                     dismiss()
                 }
             }
             .task {
-                await fetchNearbyPlaces()
+                await viewModel.fetchNearbyPlaces()
             }
         }
     }
     init(location: Location,onSave: @escaping (Location)-> Void){
-        self.location = location
+        _viewModel = StateObject(wrappedValue: ViewModel(location: location))
         self.onSave = onSave
-        _name = State(initialValue: location.name)
-        _description = State(initialValue: location.description)
     }
-    func fetchNearbyPlaces() async {
-        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
-
-        guard let url = URL(string: urlString) else {
-            print("Bad URL: \(urlString)")
-            return
-        }
-
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-
-            // we got some data back!
-            let items = try JSONDecoder().decode(Result.self, from: data)
-
-            // success – convert the array values to our pages array
-            pages = items.query.pages.values.sorted()
-            loadingState = .loaded
-        } catch {
-            // if we're still here it means the request failed somehow
-            loadingState = .failed
-        }
-    }
+    
 }
 
 struct EditView_Previews: PreviewProvider {
